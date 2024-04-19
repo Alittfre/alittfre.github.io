86bd258f5854c8e529d065a99177adfd09daca0e
Alittfre <51853302+Alittfre@users.noreply.github.com>
2024-04-19 21:06:16 +0800

    Initial commit

---
+++ .vitepress/theme/components/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="banner" :class="{ 'postViewer': state.currPost.href }" :style="`background-image: url(${banner});`">
+        <slot></slot>
+    </div>
+</template>
+<script setup lang="ts">
+import { useData } from 'vitepress';
+const themeConfig = useData().theme.value
+const banner = themeConfig.banner
+
+import { useStore } from '../store';
+const { state } = useStore()
+</script>
+<style scoped lang='less'>
+.banner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 80vh;
+    transition: all .5s;
+    z-index: 0;
+}
+
+.banner::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 30%;
+    background: linear-gradient(0.5turn, transparent, var(--general-background-color));
+}
+.postViewer {
+    height: 50vh;
+}
+</style>
